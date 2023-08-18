78284d6cd0db346824a5b62eb24ca2b155f4ef9a
kontelk <ktelkis@gmail.com>
2023-08-18 12:50:07 +0300

    Created Chuck Norris Jokes Service. Closes #3

---
+++ src/main/java/com/kt/spring5jokesapp/services/JokeService.java
@@ -0,0 +1,9 @@
+package com.kt.spring5jokesapp.services;
+
+/**
+ * Created by kontelk on 8/18/23.
+ */
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/com/kt/spring5jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,23 @@
+package com.kt.spring5jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by kontelk on 8/18/23.
+ */
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
